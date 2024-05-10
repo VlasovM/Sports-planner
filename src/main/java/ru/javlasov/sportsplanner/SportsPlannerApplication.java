81f3b7dba170437889a6bf3d327a65ec1885c20b
MaximVl <m.a.vlasov97@gmial.com>
2024-05-10 16:53:35 +0300

    [develop] Создал Spring проект;

---
+++ src/main/java/ru/javlasov/sportsplanner/SportsPlannerApplication.java
@@ -0,0 +1,13 @@
+package ru.javlasov.sportsplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javlasov/sportsplanner/SportsPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javlasov.sportsplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

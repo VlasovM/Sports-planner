247ceb72235ba20e5fa5dcacfbe55d3eb535b477
MaximVl <m.a.vlasov97@gmial.com>
2024-09-18 19:53:35 +0300

    [develop] Пытаюсь поменять анализатор sonarQube

--- Journaling/src/test/java/ru/javlasov/journaling/JournalingApplicationTests.java
+++ Journaling/src/test/java/ru/javlasov/journaling/JournalingApplicationTests.java
@@ -3,11 +3,11 @@ package ru.javlasov.journaling;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest("spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration")
-//class JournalingApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest("spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration")
+class JournalingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
